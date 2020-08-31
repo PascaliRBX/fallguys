e7ecfd972e1bcbf6cf046205a6c85edf0e0069f1
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-31 15:56:59 +0200

    Update news.js

--- cmds/news.js
+++ cmds/news.js
@@ -3,7 +3,7 @@ exports.run = async(bot, message, args) => {
 message.delete()
 const e = new Discord.RichEmbed()
 e.setTitle("News")
-e.setColor("BLUE")
+e.setColor("#084bee")
 e.setThumbnail(message.guild.iconURL)
 e.addField(`Text:`, `${args.join(' ')}`)
 e.addField("Welche Reaktion heißt was?" ,"Ja/Gut = :+1: | Nein/Schlecht = :-1:")
