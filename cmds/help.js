d9361cae9ed81bce29ee5717fe108b6a5c7f3ade
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-31 16:15:09 +0200

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,5 +1,5 @@
 const discord = require("discord.js")
-exports.run = async (bot , message , args) => {
+exports.run = (bot , message , args) => {
     let prefix = "fg!"
     let embed = new discord.RichEmbed();
     embed.setTitle("Help Übersicht")
@@ -8,7 +8,7 @@ exports.run = async (bot , message , args) => {
     embed.addField(`${prefix}kick` , "Kickt einen User")
     embed.addField(`${prefix}ban` , "Bannt einen User")
     embed.addField(`${prefix}news {Nachricht}` , "Erstellt eine Embed Message und hängt folgende Reaktionen ran: :+1: und :-1:")
-    embed.addField(`${prefix}dm @user {Nachricht} , "Sendet eine Private Nachricht an den User.")
+    embed.addField(`${prefix}dm @user {Nachricht}` , "Sendet eine Private Nachricht an den User.")
     embed.setColor("GREEN")
     message.channel.send(embed)
 }
