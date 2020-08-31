a03808339f0773f16949411ae4be3077b23d18d2
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-31 15:54:44 +0200

    Update news.js

--- cmds/news.js
+++ cmds/news.js
@@ -5,7 +5,7 @@ const e = new Discord.RichEmbed()
 e.setTitle("News")
 e.setColor("RANDOM")
 e.setThumbnail(message.guild.iconURL)
-e.addField(`\u200b`, `${args.join(' ')}`)
+e.addField(`Text:`, `${args.join(' ')}`)
 e.addField("Welche Reaktion heißt was?" ,"Ja/Gut = :+1: | Nein/Schlecht = :-1:")
 e.setTimestamp()
 message.channel.send(e).then(ch => {
