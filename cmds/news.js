cd009d8382eb6d8db23a34d44fff4a061828bc3a
PascaliRBX <68008885+PascaliRBX@users.noreply.github.com>
2020-08-31 15:15:33 +0200

    Create news.js

---
+++ cmds/news.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+exports.run = async(bot, message, args) => {
+message.delete()
+const e = new Discord.RichEmbed()
+e.setTitle("News")
+e.setColor("RANDOM")
+e.setThumbnail(message.guild.iconURL)
+e.addField(`\u200b`, `${args.join(' ')}`)
+e.setTimestamp()
+message.channel.send().then(ch => {
+await ch.react("👍")
+await ch.react("👎")
+})
+}
